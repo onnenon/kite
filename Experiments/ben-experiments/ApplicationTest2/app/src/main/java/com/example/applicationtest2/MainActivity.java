63c45d9eaa7aefcb29963ff6ed74d849e4404449
bhuinker <bhuinker@iastate.edu>
2019-01-25 22:59:03 -0600

    Center string is changed when the send button is pressed.

--- Experiments/ben-experiments/ApplicationTest2/app/src/main/java/com/example/applicationtest2/MainActivity.java
+++ Experiments/ben-experiments/ApplicationTest2/app/src/main/java/com/example/applicationtest2/MainActivity.java
@@ -5,11 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.Toast;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,13 +31,15 @@ public class MainActivity extends AppCompatActivity {
         */
 
         Button sendButton = (Button) findViewById (R.id.send_button);
+        final TextView centerText = (TextView) findViewById (R.id.center_string);
+        
 
         sendButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(getApplicationContext(), "It works!", Toast.LENGTH_LONG).show();
+                centerText.setText("It works!");
             }
         });
     }
