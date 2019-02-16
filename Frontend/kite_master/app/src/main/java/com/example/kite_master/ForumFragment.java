b777e5c0413f90019107b0fdf6c414fa088fe515
joshberg <joshberg@iastate.edu>
2019-02-16 15:56:39 -0600

    clean up volley code and move responses to Toasts

--- Frontend/kite_master/app/src/main/java/com/example/kite_master/ForumFragment.java
+++ Frontend/kite_master/app/src/main/java/com/example/kite_master/ForumFragment.java
@@ -1,9 +1,8 @@
 package com.example.kite_master;
 
 
-//standard imports
+//android imports
 
-import android.media.ExifInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,39 +13,38 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 //json imports
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 //volley imports
 import com.android.volley.AuthFailureError;
-import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+//java imports
 import java.io.UnsupportedEncodingException;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 
 public class ForumFragment extends Fragment implements View.OnClickListener {
 
-    //textviews
-    private TextView output_box;
-    //textfields
-    private EditText create_uname;
+    public String LOCAL_IP_ADDRESS;
+
+    //text fields
+    private EditText create_username;
     private EditText create_pass;
     private EditText create_bio;
-    private EditText get_single_uname;
+    private EditText get_single_username;
     private EditText delete_user_text;
     //buttons
     private Button get_user_button;
@@ -65,12 +63,16 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
 
         View v = inflater.inflate(R.layout.fragment_forum, container, false);
 
+        //set ip
+        LOCAL_IP_ADDRESS = "10.0.1.2";
+
         //instantiate buttons
         get_user_button = (Button) v.findViewById(R.id.get_user_button);
         create_user_button = (Button) v.findViewById(R.id.create_user_button);
         update_user_button = (Button) v.findViewById(R.id.update_user_button);
         delete_user_button = (Button) v.findViewById(R.id.delete_user_button);
         get_single_user_button = (Button) v.findViewById(R.id.get_single_user_button);
+
         //set onclick listeners to self
         get_user_button.setOnClickListener(this);
         create_user_button.setOnClickListener(this);
@@ -83,13 +85,11 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
         modCheckBox = (CheckBox) v.findViewById(R.id.modCheckBox);
 
         //instantiate edit text
-        create_uname = (EditText) v.findViewById(R.id.create_uname);
+        create_username = (EditText) v.findViewById(R.id.create_uname);
         create_pass = (EditText) v.findViewById(R.id.create_pass);
         create_bio = (EditText) v.findViewById(R.id.create_bio);
-        get_single_uname = (EditText) v.findViewById(R.id.get_single_uname);
+        get_single_username = (EditText) v.findViewById(R.id.get_single_uname);
         delete_user_text = (EditText) v.findViewById(R.id.delete_user_textedit);
-
-        output_box = (TextView) v.findViewById(R.id.output_box);
 
         return v;
     }
@@ -98,10 +98,9 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        volleyqueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        volleyqueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()).getApplicationContext());
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Api Tests");
-
     }
 
 
@@ -114,11 +113,11 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
                 break;
             case R.id.create_user_button:
                 System.out.println("Pushed create user");
-                createUser(create_uname.getText().toString(), create_pass.getText().toString(), create_bio.getText().toString());
+                createUser(create_username.getText().toString(), create_pass.getText().toString(), create_bio.getText().toString());
                 break;
             case R.id.update_user_button:
                 System.out.println("Pushed update user");
-                updateUser(create_uname.getText().toString(), create_pass.getText().toString(), create_bio.getText().toString(), modCheckBox.isChecked(), adminCheckBox.isChecked());
+                updateUser(create_username.getText().toString(), create_pass.getText().toString(), create_bio.getText().toString(), modCheckBox.isChecked(), adminCheckBox.isChecked());
                 break;
             case R.id.delete_user_button:
                 System.out.println("Pushed delete user");
@@ -126,42 +125,43 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
                 break;
             case R.id.get_single_user_button:
                 System.out.println("Pushed get single user");
-                getSingleUser(get_single_uname.getText().toString());
+                getSingleUser(get_single_username.getText().toString());
                 break;
         }
     }
 
 
+    //VOLLEY METHODS
+
+    //get json list of all users in the db
     public void getAllUsers() {
 
-        final JSONObject responseJson = new JSONObject();
-        String URL = "http://10.0.2.2:5000/api/user";
+        String URL = "http://" + LOCAL_IP_ADDRESS + ":5000/api/user";
         JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, URL, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        output_box.setText(response.toString());
-                        System.out.println(response.toString());
+                        Toast.makeText(getActivity(), response + " ", Toast.LENGTH_LONG).show();
+                        //System.out.println(response);
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        System.out.println("ERROR");
-                        output_box.setText("Something went wrong");
-                        Log.d("Error.Response", error.toString());
+                        Toast.makeText(getActivity(), error + " ", Toast.LENGTH_LONG).show();
+                        //System.out.println("ERROR" + error.toString());
+
                     }
                 }
         );
         volleyqueue.add(getRequest);
     }
 
-
+    //send put request to update password, bio, admin and mod status
     public void updateUser(String username, String password, String bio, boolean isMod, boolean isAdmin) {
-        String URL = "http://10.0.2.2:5000/api/user/" + username;
+        String URL = "http://" + LOCAL_IP_ADDRESS + ":5000/api/user/" + username;
 
         JSONObject jsonBody = new JSONObject();
-
         try {
             jsonBody.put("password", password);
             jsonBody.put("bio", bio);
@@ -172,20 +172,19 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
         }
 
         final String requestBody = jsonBody.toString();
-
         StringRequest postRequest = new StringRequest(Request.Method.PUT, URL,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        output_box.setText(response.toString());
-                        System.out.println(response.toString());
+                        Toast.makeText(getActivity(), response + " ", Toast.LENGTH_LONG).show();
+                        //System.out.println(response);
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        output_box.setText("Error - User may already exist");
-                        Log.d("Error.Response", error.toString());
+                        Toast.makeText(getActivity(), error.toString() + " ", Toast.LENGTH_LONG).show();
+                        //System.out.println("ERROR" + error.toString());
                     }
                 }
         ) {
@@ -195,24 +194,23 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
             }
 
             @Override
-            public byte[] getBody() throws AuthFailureError {
-                try {
-                    return requestBody == null ? null : requestBody.getBytes("utf-8");
-                } catch (UnsupportedEncodingException uee) {
-                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
-                    return null;
-                }
+            public byte[] getBody() {
+                return requestBody.getBytes(StandardCharsets.UTF_8);
             }
         };
-        volleyqueue.add(postRequest);
-        create_uname.setText("");
+
+        //reset fields
+        create_username.setText("");
         create_pass.setText("");
         create_bio.setText("");
+
+        volleyqueue.add(postRequest);
+
     }
 
 
     public void createUser(String username, String password, String bio) {
-        String URL = "http://10.0.2.2:5000/api/user";
+        String URL = "http://" + LOCAL_IP_ADDRESS + ":5000/api/user";
 
         JSONObject jsonBody = new JSONObject();
 
@@ -225,20 +223,19 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
         }
 
         final String requestBody = jsonBody.toString();
-
         StringRequest postRequest = new StringRequest(Request.Method.POST, URL,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        output_box.setText(response.toString());
-                        System.out.println(response.toString());
+                        Toast.makeText(getActivity(), response + " ", Toast.LENGTH_LONG).show();
+                        //System.out.println(response);
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        output_box.setText("Error - User may already exist");
-                        Log.d("Error.Response", error.toString());
+                        Toast.makeText(getActivity(), error.toString() + " ", Toast.LENGTH_LONG).show();
+                        //System.out.println("ERROR" + error.toString());
                     }
                 }
         ) {
@@ -248,75 +245,70 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
             }
 
             @Override
-            public byte[] getBody() throws AuthFailureError {
-                try {
-                    return requestBody == null ? null : requestBody.getBytes("utf-8");
-                } catch (UnsupportedEncodingException uee) {
-                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
-                    return null;
-                }
+            public byte[] getBody() {
+                return requestBody.getBytes(StandardCharsets.UTF_8);
             }
         };
-        volleyqueue.add(postRequest);
-        create_uname.setText("");
+        //set fields back to blank
+        create_username.setText("");
         create_pass.setText("");
         create_bio.setText("");
+
+        volleyqueue.add(postRequest);
+
     }
 
 
     public void getSingleUser(String username) {
 
-        final JSONObject responseJson = new JSONObject();
-        String URL = "http://10.0.2.2:5000/api/user/" + username;
+        String URL = "http://" + LOCAL_IP_ADDRESS + ":5000/api/user/" + username;
         JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, URL, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        output_box.setText(response.toString());
-                        System.out.println(response.toString());
+                        Toast.makeText(getActivity(), response + " ", Toast.LENGTH_LONG).show();
+                        //System.out.println(response);
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        output_box.setText("This user doesnt exist");
-                        System.out.println("ERROR");
-                        Log.d("Error.Response", error.toString());
+                        Toast.makeText(getActivity(), error.toString() + " ", Toast.LENGTH_LONG).show();
+                        //System.out.println("ERROR" + error.toString());
                     }
                 }
         );
+        //set field back to blank
+        get_single_username.setText("");
+
         volleyqueue.add(getRequest);
-        get_single_uname.setText("");
 
     }
 
 
     public void deleteUser(String username) {
 
-        final JSONObject responseJson = new JSONObject();
-        String URL = "http://10.0.2.2:5000/api/user/" + username;
+        String URL = "http://" + LOCAL_IP_ADDRESS + ":5000/api/user/" + username;
         JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.DELETE, URL, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        output_box.setText(response.toString());
-                        System.out.println(response.toString());
+                        Toast.makeText(getActivity(), response + " ", Toast.LENGTH_LONG).show();
+                        //System.out.println(response);
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        output_box.setText("This user doesnt exist");
+                        Toast.makeText(getActivity(), error.toString() + " ", Toast.LENGTH_LONG).show();
                         System.out.println("ERROR");
-                        Log.d("Error.Response", error.toString());
+                        //System.out.println("ERROR" + error.toString());
                     }
                 }
         );
-        volleyqueue.add(getRequest);
+        //set fields back to blank
         delete_user_text.setText("");
 
-
+        volleyqueue.add(getRequest);
     }
-
-
 }
