7ad79f9d781f48f15a20f75e61f11d94de1cd6e1
bhuinker <bhuinker@iastate.edu>
2019-02-16 17:06:35 -0600

    Functionality to change all possible variables with a PUT request added.

--- Frontend/HTTPUserCommunication/app/src/main/java/com/example/httpusercommunication/MainActivity.java
+++ Frontend/HTTPUserCommunication/app/src/main/java/com/example/httpusercommunication/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.httpusercommunication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,22 +9,15 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
