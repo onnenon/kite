8b21e13b07f1ae600a9d27eb9c6799131abcda03
joshberg <joshberg@iastate.edu>
2019-03-05 17:31:29 -0600

    gradle update and forum list view add

--- Frontend/kite_master/app/src/main/java/com/team100/kite_master/forum/ForumFragment.java
+++ Frontend/kite_master/app/src/main/java/com/team100/kite_master/forum/ForumFragment.java
@@ -6,16 +6,57 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import com.team100.kite_master.R;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
 public class ForumFragment extends Fragment {
 
+
+
+    ListView listView;
+    List alist = new ArrayList();
+    ArrayAdapter adapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_forum, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_forum, container, false);
+
+        listView = (ListView) v.findViewById(R.id.list_view);
+
+        alist.add("one");
+        alist.add("two");
+        alist.add("three");
+        alist.add("four");
+        alist.add("five");
+        alist.add("six");
+        alist.add("seven");
+        alist.add("seven");
+        alist.add("seven");
+        alist.add("seven");
+        alist.add("seven");
+        alist.add("seven");
+        alist.add("seven");
+        alist.add("seven");
+        alist.add("seven");
+        alist.add("seven");
+
+
+
+        adapter = new ArrayAdapter(Objects.requireNonNull(getActivity()).getApplicationContext(), android.R.layout.simple_list_item_1, alist);
+        listView.setAdapter(adapter);
+
+
+
+        return v;
     }
 
 
