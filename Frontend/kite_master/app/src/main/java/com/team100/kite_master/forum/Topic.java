13708b6ff3b2c9848f1e79e3b4a0aa1c1edf23a8
joshberg <joshberg@iastate.edu>
2019-03-06 15:16:46 -0600

    remove refactored network class, clean up topic class

--- Frontend/kite_master/app/src/main/java/com/team100/kite_master/forum/Topic.java
+++ Frontend/kite_master/app/src/main/java/com/team100/kite_master/forum/Topic.java
@@ -6,48 +6,45 @@ public class Topic implements Comparable<Topic>{
     private String topicDescription;
     private String topicID;
 
+    //constructor for topics
     public Topic(String name, String description) {
         topicID = name;
         topicName = formatName(name);
         topicDescription = description;
 
     }
+    //returns topic id (unformatted name)
+    public String getTopicID() { return topicID; }
 
-    public String getTopicID() {
-        return topicID;
-    }
+    //returns topic name (formatted)
+    public String getName() { return topicName; }
 
-    public String getName() {
-        return topicName;
-    }
-
-    @Override
-    public int compareTo(Topic comparetopic) {
-        int cmp= this.getName().compareTo(comparetopic.getName());
-        /* For Ascending order*/
-        return cmp;
-    }
-
-
+    //returns description of topic
     public String getDescription() {
         return topicDescription;
     }
 
-    public String toString(){
-        return topicID+","+topicDescription;
+    //overrides compare to to allow alphabetical sorting of the topic list
+    @Override
+    public int compareTo(Topic comparetopic) {
+        int cmp = this.getName().compareTo(comparetopic.getName());
+        return cmp;
     }
 
+    //returns a string containing the topic id and description
+    public String toString(){
+        return "|" + topicID+" -> "+topicDescription+"|";
+    }
 
+    //formats the name so only the first letter of each word is uppercase
     private String formatName(String name) {
         String str = name.toLowerCase();
         StringBuilder s = new StringBuilder(str.length());
         String words[] = str.split(" ");
         for (int i = 0; i < words.length; i++) {
             s.append(Character.toUpperCase(words[i].charAt(0))).append(words[i].substring(1)).append(" ");
-
         }
         return s.toString();
     }
-
 
 }
