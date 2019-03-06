86d1e179f4f399fb3e52ed4807a0ab984323cb60
joshberg <joshberg@iastate.edu>
2019-03-05 19:41:34 -0600

    add list view with custom layout items

--- Frontend/kite_master/app/src/main/java/com/team100/kite_master/forum/ForumFragment.java
+++ Frontend/kite_master/app/src/main/java/com/team100/kite_master/forum/ForumFragment.java
@@ -7,7 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.team100.kite_master.R;
 
@@ -17,7 +20,6 @@ import java.util.Objects;
 
 
 public class ForumFragment extends Fragment {
-
 
 
     ListView listView;
@@ -50,10 +52,10 @@ public class ForumFragment extends Fragment {
         alist.add("seven");
 
 
+        CustomAdapter customAdapter = new CustomAdapter();
 
         adapter = new ArrayAdapter(Objects.requireNonNull(getActivity()).getApplicationContext(), android.R.layout.simple_list_item_1, alist);
-        listView.setAdapter(adapter);
-
+        listView.setAdapter(customAdapter);
 
 
         return v;
@@ -65,4 +67,39 @@ public class ForumFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Objects.requireNonNull(getActivity()).setTitle("Forum");
     }
+
+    class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return alist.size(); //TODO
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.topic_list_item, null);
+
+            TextView topicTitle = (TextView) view.findViewById(R.id.text_title);
+            TextView topicDescription = (TextView) view.findViewById(R.id.text_description);
+
+            topicTitle.setText(alist.get(i).toString());
+            topicDescription.setText(alist.get(i).toString());
+
+            return view;
+
+        }
+
+    }
+
+
 }
