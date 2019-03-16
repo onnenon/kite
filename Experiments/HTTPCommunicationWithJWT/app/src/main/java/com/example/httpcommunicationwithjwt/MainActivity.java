a53063a5728b75f82973301b28223a0acac6f80a
bhuinker <bhuinker@iastate.edu>
2019-03-15 23:31:25 -0500

    Creating a setup that gets a JWT for fadmin specifically.

---
+++ Experiments/HTTPCommunicationWithJWT/app/src/main/java/com/example/httpcommunicationwithjwt/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.httpcommunicationwithjwt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button SignInButton;
+    private TextView LoginJWT;
+
+    private String URL = "http://kite.onn.sh/api/auth/login";
+    private RequestQueue Requests;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SignInButton = (Button) findViewById(R.id.SignIn);
+        LoginJWT = (TextView) findViewById(R.id.LoginStatus);
+
+        Requests = Volley.newRequestQueue(this);
+
+        SignInButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String username = "fadmin";
+                String password = "pass";
+
+                login(username, password);
+            }
+        });
+    }
+
+    private void login(final String username, final String password) {
+
+        JSONObject LoginCredentials = new JSONObject();
+
+        try {
+            LoginCredentials.put("Username", username);
+            LoginCredentials.put("Password", password);
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest loginRequest = new JsonObjectRequest(Request.Method.POST, URL, null,
+
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+
+                            JSONObject token = response.getJSONObject("data");
+
+                            String JWT = token.getString("access_token");
+
+                            LoginJWT.setText(JWT);
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        Toast.makeText(getApplication(), response + "", Toast.LENGTH_SHORT).show();
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        LoginJWT.setText("Username or password incorrect?");
+
+                        Toast.makeText(MainActivity.this, error + "", Toast.LENGTH_SHORT).show();
+                    }
+                })
+
+        {
+            // Credit to the people at this source: https://stackoverflow.com/questions/44000212/how-to-send-authorization-header-in-android-using-volley-library
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+
+                String credentials = username + ":" + password;
+                String base64EncodedCredentials = Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+
+                HashMap<String, String> headers = new HashMap<String, String>();
+
+                headers.put("Authorization", "Basic " + base64EncodedCredentials);
+
+                return headers;
+            }
+        };
+
+        Requests.add(loginRequest);
+    }
+}
