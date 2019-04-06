1b543a400e8bc05dac18c435d529ed72c534a8ad
joshberg <joshberg@iastate.edu>
2019-04-06 15:44:09 -0500

    add dateutil unit test class

---
+++ Frontend/kite_master/app/src/test/java/com/example/kite_master/DateUtilTest.java
@@ -0,0 +1,28 @@
+package com.example.kite_master;
+import com.team100.kite_master.forum.forum_data_classes.DateUtil;
+
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+public class DateUtilTest {
+
+    private DateUtil d;
+
+
+    @Before
+    public void setup(){
+        d = new DateUtil();
+    }
+
+
+
+
+
+
+}
