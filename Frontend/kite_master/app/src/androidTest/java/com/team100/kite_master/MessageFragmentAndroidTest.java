add7d19d5b3b5ef388446e39a374f30d6f66bbe5
bhuinker <bhuinker@iastate.edu>
2019-04-28 23:22:53 -0500

    I might give up on writing the JUnit tests.

--- Frontend/kite_master/app/src/androidTest/java/com/team100/kite_master/MessageFragmentAndroidTest.java
+++ Frontend/kite_master/app/src/androidTest/java/com/team100/kite_master/MessageFragmentAndroidTest.java
@@ -1,39 +1,28 @@
 package com.team100.kite_master;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
+import android.os.Looper;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
-import android.view.WindowManager;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-import com.team100.kite_master.favorites.FavoriteStorageHandler;
-import com.team100.kite_master.forum.forum_data_classes.Post;
-import com.team100.kite_master.login.SaveSharedPreference;
 import com.team100.kite_master.messages.MessagesFragment;
 import com.team100.kite_master.messages.WebSocketImplementation;
 import com.team100.kite_master.messages.messages_data_classes.Message;
 import com.team100.kite_master.messages.messages_data_classes.MessageLayoutSetup;
-import com.team100.kite_master.networking.NetworkManager;
-import com.team100.kite_master.userdata.User;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
-public class MessageFragmentAndroidTest extends MainActivity {
+public class MessageFragmentAndroidTest {
 
     // Test variables
+    private MainActivity main;
     private MessagesFragment messageFrag;
     private WebSocketImplementation impWS;
     private Message realMessage;
@@ -45,15 +34,16 @@ public class MessageFragmentAndroidTest extends MainActivity {
 
 
 
-    /*
+    // Credit to this source: https://stackoverflow.com/questions/46458735/instrumented-unit-class-test-cant-create-handler-inside-thread-that-has-not-c
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
     // Setup
     @Before
-    public void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
+    public void onCreate() {
 
         // Regular objects
+
         messageFrag = new MessagesFragment();
         messageFrag.setUsername("fadmin");
         messageFrag.setIPaddress("kite.onn.sh");
@@ -70,18 +60,18 @@ public class MessageFragmentAndroidTest extends MainActivity {
     @Test
     public void MessageLayoutSetupInitializationTest() {
 
-        messageFrag.setUsername("UserOne");
+        // messageFrag.setUsername("UserOne");
 
-        layoutSetup = new MessageLayoutSetup(messageFrag.getContext(), messageFrag.getUsername());
+        // layoutSetup = new MessageLayoutSetup(messageFrag.getContext(), messageFrag.getUsername());
 
         // Make sure that the instance variables of layoutSetup and messageFrag are the same.
-        Assert.assertNotNull(messageFrag.getContext());
-        Assert.assertNotNull(layoutSetup.getContext());
-        assertEquals(layoutSetup.getContext(), messageFrag.getContext());
-        assertEquals(layoutSetup.getUsername(), messageFrag.getUsername());
+        // Assert.assertNotNull(messageFrag.getContext());
+        // Assert.assertNotNull(layoutSetup.getContext());
+        // assertEquals(layoutSetup.getContext(), messageFrag.getContext());
+        // assertEquals(layoutSetup.getUsername(), messageFrag.getUsername());
     }
 
-    */
+
 
     /*
 
